a5ab037b2fc4b29b6792a214cbd9c119d5ebb661
shintan777 <tanvi.shinde46@gmail.com>
2019-03-21 22:00:26 +0530

    Inserts text to text-widget(english)

--- stt_mix.py
+++ stt_mix.py
@@ -1,35 +1,47 @@
+from tkinter import *
+from stt import *
 import speech_recognition as sr
 import pyaudio
-from tkinter import *
-lang='en-US'
 root =Tk()
-
-
-def to_hin():
-    lang='hi-IN'
-def to_eng():
-    lang='en-US'
 def text_eng():
     r  = sr.Recognizer()
     with sr.Microphone() as source:
         text="..."
         r.energy_threshold = 4000
         try:
-            while(text!='stop'):
+            while(text!='enter'):
+                print(text)
                 otpt.insert(INSERT,text)
                 otpt.insert(INSERT,'\n')
-                audio = r.listen(source) 
-                text = r.recognize_google(audio)  
-        except: 
+                otpt.pack()
+                audio = r.listen(source) #Starts Listening
+                text = r.recognize_google(audio) #Recognizes audio in English 
+        except: #When there is no notable speech
             print("Sorry, couldn't hear you!")
+def text_hin():
+    r  = sr.Recognizer()
+    with sr.Microphone() as source:
+        text="Speak now"
+        ans=''
+        r.energy_threshold = 4000
+        try:
+            while(text!='enter'):
+                print(text)
+                otpt.insert(INSERT,text)
+                otpt.insert(INSERT,'\n')
+                otpt.pack()
+                audio = r.listen(source) #Starts Listening
+                text = r.recognize_google(audio,language='hi-IN') #Recognizes audio in Hindi
+        except: #When there is no notable speech
+            print("Sorry, couldn't hear you!") 
+def to_hin():
+    lang='hi-IN'
+def to_eng():
+    lang='en-US'
 
-
-#text-field
-otpt=Text(root)
 
 #Heading
-head_txt="Talk to type"
-Head=Label(root,text=head_txt,bg="Black",fg="white")
+Head=Label(root,text="Talk to type",bg="Black",fg="white")
 Head.pack(fill=X,pady=10)
 
 topFrame=Frame(root)
@@ -41,10 +53,10 @@ bottomFrame.pack()
 button1=Button(topFrame,text="CLICK TO BEGIN",fg="red",command=text_eng)
 button1.pack()
 
-eng=Button(topFrame,text="ENGLISH",fg="Blue",command=to_eng)
+eng=Button(topFrame,text="ENGLISH",fg="Blue",command=text_eng)
 eng.pack(side="left",padx=10,pady=10)
 
-hin=Button(topFrame,text="HINDI",fg="Purple",command=to_hin)
+hin=Button(topFrame,text="HINDI",fg="Purple",command=text_hin)
 hin.pack(side="left",padx=10,pady=10)
 
 #checkbox
@@ -55,6 +67,9 @@ c1.pack(padx=10,pady=10)
 c2=Checkbutton(root,text="Convert into text file")
 c2.pack(padx=10,pady=10)
 
-#pack
+
+
+#text-field
+otpt=Text(root)
 otpt.pack()
 root.mainloop()
