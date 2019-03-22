17cc2af1015d62feb0f0dc392337cb6e365bfbdf
shintan777 <tanvi.shinde46@gmail.com>
2019-03-22 20:12:45 +0530

    A few additions

--- stt_mix.py
+++ stt_mix.py
@@ -3,10 +3,13 @@ from stt import *
 import speech_recognition as sr
 import pyaudio
 root =Tk()
+
 def text_eng():
+    Head['text']='Start talking'
+    Head.pack()
+    text=''
     r  = sr.Recognizer()
     with sr.Microphone() as source:
-        text="..."
         r.energy_threshold = 4000
         try:
             while(text!='enter'):
@@ -14,30 +17,49 @@ def text_eng():
                 otpt.insert(INSERT,text)
                 otpt.insert(INSERT,'\n')
                 otpt.pack()
-                audio = r.listen(source) #Starts Listening
-                text = r.recognize_google(audio) #Recognizes audio in English 
-        except: #When there is no notable speech
-            print("Sorry, couldn't hear you!")
+                audio = r.listen(source) 
+                text = r.recognize_google(audio)  
+        except: 
+            Head['text']='Sorry, couldnt hear you! Try again'
+            Head.pack()
+
+                
+    
 def text_hin():
+    Head['text']='Start talking'
+    Head.pack()
+    text=""
     r  = sr.Recognizer()
     with sr.Microphone() as source:
-        text="Speak now"
-        ans=''
-        r.energy_threshold = 4000
+        text=""
+        r.energy_threshold = 1000
         try:
-            while(text!='enter'):
+            while(text!='रुक'):
                 print(text)
                 otpt.insert(INSERT,text)
                 otpt.insert(INSERT,'\n')
                 otpt.pack()
-                audio = r.listen(source) #Starts Listening
-                text = r.recognize_google(audio,language='hi-IN') #Recognizes audio in Hindi
-        except: #When there is no notable speech
-            print("Sorry, couldn't hear you!") 
-def to_hin():
-    lang='hi-IN'
-def to_eng():
-    lang='en-US'
+                audio = r.listen(source) 
+                text = r.recognize_google(audio,language='hi-IN')
+        except: 
+            Head['text']='Sorry, couldnt hear you! Try again'
+            Head.pack()
+
+
+def create_file():
+    #name=str(E.get('1',END))
+    name=E1.get()
+    name=name+'.txt'
+    stuff=otpt.get("1.0",END)
+    f=open(name,'w+')
+    f.write(stuff)
+    f.close
+    Head['text']='Created'
+    Head.pack()
+    
+def copy():
+    stuff=otpt.get("1.0",END)
+    
 
 
 #Heading
@@ -50,8 +72,8 @@ bottomFrame=Frame(root)
 bottomFrame.pack()
 
 #button
-button1=Button(topFrame,text="CLICK TO BEGIN",fg="red",command=text_eng)
-button1.pack()
+#button1=Button(topFrame,text="CLICK TO BEGIN",fg="red",command=text_eng)
+#button1.pack()
 
 eng=Button(topFrame,text="ENGLISH",fg="Blue",command=text_eng)
 eng.pack(side="left",padx=10,pady=10)
@@ -61,15 +83,24 @@ hin.pack(side="left",padx=10,pady=10)
 
 #checkbox
 
-c1=Checkbutton(root,text="COPY")
+c1=Checkbutton(bottomFrame,text="COPY")
 c1.pack(padx=10,pady=10)
 
-c2=Checkbutton(root,text="Convert into text file")
-c2.pack(padx=10,pady=10)
+#c2=Checkbutton(root,text="Convert into text file")
+#c2.pack(padx=10,pady=10)
 
+ctf=Button(bottomFrame,text="Convert to txt file",fg="red",command= lambda :  create_file())
+ctf.pack(side="left",padx=10,pady=10)
 
+E1 = Entry(bottomFrame, bd =5)
+E1.pack(side="left",padx=10,pady=10)
 
 #text-field
 otpt=Text(root)
-otpt.pack()
+otpt.pack(side="bottom")
+
+
 root.mainloop()
+
+
+
